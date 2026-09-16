1b6be98d07915b18744bfeb4ba41de9002333ece
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-16 19:55:03 +0100

    Complete GitHub to Firebase Palo account flow

--- addons/palo/plugin.gd
+++ addons/palo/plugin.gd
@@ -28,20 +28,45 @@ func _enter_tree():
     panel = preload("res://addons/palo/palo_panel.gd").new()
     github.setup(panel)
     panel.setup(git, github, settings)
-    panel.setup_cloud(firebase_auth, cloud_service)
     add_control_to_dock(DOCK_SLOT_RIGHT_UL, panel)
+
+    github.connect("auth_succeeded", self, "_on_github_auth_succeeded")
+    firebase_auth.connect("auth_succeeded", self, "_on_firebase_auth_succeeded")
+    firebase_auth.connect("auth_failed", self, "_on_firebase_auth_failed")
 
 func _load_firebase_user_config():
     var path = "user://palo_firebase.json"
-    if not File.new().file_exists(path):
-        return
     var file = File.new()
+    if not file.file_exists(path):
+        return
     if file.open(path, File.READ) != OK:
         return
     var parsed = JSON.parse(file.get_as_text())
     file.close()
     if parsed.error == OK and typeof(parsed.result) == TYPE_DICTIONARY:
         firebase_auth.configure(parsed.result)
+
+func _on_github_auth_succeeded(access_token):
+    if panel:
+        panel._set_status("GitHub connected. Creating/checking your Palo account...")
+    if not firebase_auth.is_configured():
+        if panel:
+            panel._set_status("GitHub connected. Firebase is not configured yet. Add user://palo_firebase.json using docs/firebase-setup.md, then reconnect.")
+        return
+    cloud_service.sign_in_with_github_token(access_token)
+
+func _on_firebase_auth_succeeded(user_data, id_token):
+    palo_account.set_firebase_user(user_data)
+    cloud_service.save_current_profile({
+        "github_username": str(user_data.get("github_username", "")),
+        "platform": "palo"
+    })
+    if panel:
+        panel._set_status("Palo account connected as " + str(user_data.get("email", user_data.get("display_name", "user"))) + ". GitHub remains connected for repository access.")
+
+func _on_firebase_auth_failed(message):
+    if panel:
+        panel._set_status("GitHub is connected, but Palo Firebase account sign-in failed: " + str(message))
 
 func _exit_tree():
     if panel:
