414106ff3b6d45a1024375e0c62de1148cc492c2
ojdevsent-lgtm <ojdevs.ent@gmail.com>
2026-09-16 01:12:12 +0100

    Connect Palo editor plugin to collaboration core

--- addons/palo/plugin.gd
+++ addons/palo/plugin.gd
@@ -1,29 +1,19 @@
 tool
 extends EditorPlugin
 
-var panel
+var panel = null
+var git = null
+var status_label = null
 
 func _enter_tree():
-	panel = VBoxContainer.new()
-
-	var title = Label.new()
-	title.text = "Palo"
-	panel.add_child(title)
-
-	var upload_btn = Button.new()
-	upload_btn.text = "Upload My Work"
-	panel.add_child(upload_btn)
-
-	var update_btn = Button.new()
-	update_btn.text = "Get Team Updates"
-	panel.add_child(update_btn)
-
-	var backup_btn = Button.new()
-	backup_btn.text = "Create Backup"
-	panel.add_child(backup_btn)
-
+	git = preload("res://addons/palo/git_manager.gd").new()
+	panel = preload("res://addons/palo/palo_panel.gd").new()
+	panel.setup(git)
 	add_control_to_dock(DOCK_SLOT_RIGHT_UL, panel)
 
 func _exit_tree():
-	remove_control_from_docks(panel)
-	panel.free()
+	if panel:
+		remove_control_from_docks(panel)
+		panel.queue_free()
+	panel = null
+	git = null
